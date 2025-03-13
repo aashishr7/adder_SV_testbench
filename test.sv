a05c01dce9e92e8beabef4285f988ed12c3b9126
AASHISH RANJAN <44123630+aashishr7@users.noreply.github.com>
2025-03-14 01:20:58 +0530

    Create test.sv

---
+++ test.sv
@@ -0,0 +1,20 @@
+`include "environment.sv"
+
+
+program test(intf i_intf);
+  
+  //declaring environment instance
+  environment env;
+  
+  initial begin
+    //creating environment
+    env = new(i_intf);
+    
+    //setting the repeat count of generator as 4, means to generate 4 packets
+    env.gen.count = 34;
+    
+    //calling run of env, it interns calls generator and driver main tasks.
+    env.run();
+  end
+  
+endprogram
